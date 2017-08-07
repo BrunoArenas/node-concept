92254d60f399a1b95f244a0e1197286be93c2769
yuriadams <yuriadams@gmail.com>
2017-08-07 15:34:05 -0300

    customizando sequelize para executar de acordo com o modelo de seu projeto

---
+++ api/config/sequelize.js
@@ -0,0 +1,21 @@
+{
+  "development": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "concept_development",
+    "host": "localhost",
+    "dialect": "postgres",
+    "protocol": "postgres"
+  },
+  "test": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "concept_test",
+    "host": "localhost",
+    "dialect": "postgres",
+    "protocol": "postgres"
+  },
+  "production": {
+    "use_env_variable": "DATABASE_URL"
+  }
+}
